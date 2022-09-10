900d4633fe3aabbe82e124e64a162e4e66175565
DKrkc <92672947+DKrkc@users.noreply.github.com>
2022-09-10 18:17:03 +0300

    Add Authentication

--- src/Kodlama.io.Devs/WebAPI/Program.cs
+++ src/Kodlama.io.Devs/WebAPI/Program.cs
@@ -1,6 +1,7 @@
 using Application;
 using Core.CrossCuttingConcerns.Exceptions;
 using Core.Security.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,7 +21,20 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = tokenOptions.Issuer,
+            ValidAudience = tokenOptions.Audience,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
+        };
+    });
 
 var app = builder.Build();
 
